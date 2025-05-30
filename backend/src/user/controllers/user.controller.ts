7658bf278481c079ac482f210e509fc040637b0e
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2025-05-30 18:17:57 -0300

    fix: removing backend find me endpoint

--- backend/src/user/controllers/user.controller.ts
+++ backend/src/user/controllers/user.controller.ts
@@ -1,20 +1,10 @@
 import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateUserRequestDto } from '../dtos/requests/create-user.request-dto';
 import { UserService } from '../services/user.service';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
-import { User } from '../entities/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @UseGuards(JwtAuthGuard)
-  @Get('me')
-  getProfile(@CurrentUser() user: User): Omit<User, 'password'> {
-    const { password, ...result } = user;
-    return result;
-  }
 
   @Post()
   create(@Body() createUserDto: CreateUserRequestDto): Promise<void> {
