4e38c19949533cf01b0a1cd7953a018ec8ea2401
Matheus Grego <matheusgrego6@gmail.com>
2025-11-24 20:16:52 -0300

    fix/ voltei a versao do matchmaking-response e usematchmaking,  criei a versao do whoami delas

--- mobile/models/responses/Whoami-matchmaking-response.ts
+++ mobile/models/responses/Whoami-matchmaking-response.ts
@@ -3,12 +3,14 @@ import { MatchFormat } from "../types/match-format.type";
 import { MatchLanguage } from "../types/match-language.type";
 import { MatchMode } from "../types/match-mode.type";
 
-export default interface IMatchmakingResponse {
+export default interface IWHOAMIMatchmakingResponse {
   message: string;
   matchMode: MatchMode;
   matchFormat: MatchFormat;
   language: MatchLanguage;
   matchId: string;
   isOfferer: boolean;
-  buddy: IUserBuddy
+  buddy: IUserBuddy;
+  timerStartTimestamp?: number;
+  timerDurationMs?: number;
 }
